4f35ce8dcad5edbbed19ccc3b3025f72060fdad6
hackamro <lounis.amar.unity@gmail.com>
2022-12-12 08:34:55 +0100

    Signed-off-by: hackamro <lounis.amar.unity@gmail.com>

--- app/src/main/java/com/app/picnic/MainActivity.java
+++ app/src/main/java/com/app/picnic/MainActivity.java
@@ -2,42 +2,26 @@ package com.app.picnic;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.app.picnic.databinding.ActivityMainBinding;
-import com.app.picnic.viewmodels.WeatherViewModel;
+import android.widget.Toast;
+import com.app.picnic.views.WeatherActivity;
 
 public class MainActivity extends AppCompatActivity {
-    WeatherViewModel vm_weather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        vm_weather = new ViewModelProvider(this).get(WeatherViewModel.class);
-
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
-        binding.setVm(vm_weather);
-        binding.setLifecycleOwner(this);
-
-//        vm_weather.mlds_name.observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(String s) {
-//
-//            }
-//        });
-
-
-//        binding.btRun.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(getApplicationContext(),"toast",Toast.LENGTH_LONG).show();
-//                binding.tvName.setText("hello");
-//            }
-//        });
+        try{
+            Intent i = new Intent(getApplicationContext(), WeatherActivity.class);
+            startActivity(i);
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(),"error",Toast.LENGTH_LONG).show();
+        }
     }
+
 }
--- app/src/main/java/com/app/picnic/views/WeatherActivity.java
+++ app/src/main/java/com/app/picnic/views/WeatherActivity.java
@@ -1,10 +1,14 @@
 package com.app.picnic.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 import com.app.picnic.R;
+import com.app.picnic.databinding.ActivityWeatherBinding;
+import com.app.picnic.viewmodels.WeatherViewModel;
 
 public class WeatherActivity extends AppCompatActivity {
 
@@ -13,6 +17,9 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-
+        WeatherViewModel vm_weather = new ViewModelProvider(this).get(WeatherViewModel.class);
+        ActivityWeatherBinding binding= DataBindingUtil.setContentView(this,R.layout.activity_weather);
+        binding.setVm(vm_weather);
+        binding.setLifecycleOwner(this);
     }
 }
